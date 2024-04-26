e5bb784ec788bfbefa40920a5a2d3b844dda3c90
Denis Wojtkowiak <denis.wojtkowiak@rst.com.pl>
2024-04-26 23:21:03 +0200

    Mnist WIP

--- mnist.py
+++ mnist.py
@@ -1,1 +1,38 @@
+import os.path
+
 import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train.reshape((60000, 28, 28, 1))
+x_test = x_test.reshape((10000, 28, 28, 1))
+
+# check if model already exists
+try:
+    model_cnn = tf.keras.models.load_model('model_cnn.keras')
+
+model_cnn = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model_cnn.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+model_cnn.fit(x_train, y_train, epochs=5)
+
+test_loss, test_acc = model_cnn.evaluate(x_test, y_test)
+print(f"Test accuracy: {test_acc}")
+
+model_cnn.save('model_cnn.keras')
+
+model_cnn.summary()
